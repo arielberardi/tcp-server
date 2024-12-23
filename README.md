da6fea5f996f15851c2b5d7b2dcb7225a01da8ca
Ariel Berardi <ariel.berardi95@gmail.com>
2024-12-23 07:45:06 +0000

    Setup Template

---
+++ README.md
@@ -0,0 +1,12 @@
+# HTTP Server 
+
+## Setup & Run
+
+Requirement: cmake and some c++ compiler (gcc/clang, etc)
+
+1) From root folder run: cmake -S . -B build -DCMAKE_BUILD_TYPE=Release (or Debug)
+2) Then ./build/http-server 
+
+## Contact
+
+For any questions or suggestions, please reach out [Ariel Berardi](https://www.linkedin.com/in/aberardi95/).
---
+++ Source/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Step 1: Search about HTTP Server and how they work
+// Step 2: Build a Socket that can received TCP Connection
+// Step 3: Reply to the Socket connection with the right answer
+// Step 4: Add a threading system to handle multiple connections
+// Step 5: Add a HTTP Parser so I can read the HTTP request and reply to it
+
+int main() { std::cout << "Hello HTTP Server\n"; }
